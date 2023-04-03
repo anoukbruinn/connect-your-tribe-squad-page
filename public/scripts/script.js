79d8685e511dd56c08c5ca01dbf24d2d4041eaee
anoukbruinn <112856687+anoukbruinn@users.noreply.github.com>
2023-04-03 10:38:25 +0200

    done
    done

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -3,18 +3,6 @@
 // select all like buttons
 const likeBtns = document.querySelectorAll('.like-btn');
 
-// loop through all like buttons and add event listener
-likeBtns.forEach((btn, index) => {
-  btn.addEventListener('click', () => {
-    // toggle the 'like' and 'fill-heart' classes on the clicked button
-    btn.classList.toggle('on');
-    btn.classList.toggle('fill-heart');
-
-    // add a unique animation class based on the button's index
-    btn.classList.add(`animate-${index + 1}`);
-  });
-});
-
 // Looks at the form and places the button when the user like someone
 const form = document.querySelector('form');
 const submitBtn = document.querySelector('#submit-btn');
